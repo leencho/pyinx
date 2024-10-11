8f260b71612f9cbb6908bae3da93a5ebed777fe8
leencho <ahmed567moha@gmail.com>
2024-10-11 18:00:41 +0200

    added path

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ class Http_server():
         self.address=address
         self.port=port
         self.request = {}
-        self.response=""
+
     def run(self):
         server=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server.bind((self.address, self.port))
@@ -18,9 +18,13 @@ class Http_server():
         fd, client= server.accept()
         print(f"received connection from {client}")
         while True:
-            self.response={}
             data = fd.recv(1024)
-            data=data.decode().strip().split(' ')
+            data = data.decode()
+            data=data.split()
+            #data=data.strip().split(' ')
+            print(data)
+            if not data:
+                self.unsupported_http_version()
             self.request = {"method": data[0], "path": data[1], "version": data[2]}
             if self.request['version'] == "HTTP/1.1" or self.request['version']=='HTTP/1.0':
                 if self.request['method']=='GET':
@@ -41,13 +45,29 @@ class Http_server():
 
     
     def get_request(self, fd):
+        body=b"<h1> Well come </h1>"
         if self.request['path']=='/':
             header=b"HTTP/1.1 200 ok\r\n"
+            header+=f"Host: {self.address}\r\n".encode('utf-8')
+            header +=b'Content-type: text/html\r\n'
+            content_len=len(body)
+            header += f"Content-Length: {content_len}\r\n".encode('utf-8')
+            header += b"\r\n"
+            header+=body
             print(header)
             fd.send(header)
+        elif self.request['path']=='/index':
+            header=b"HTTP/1.1 200 ok\r\n"
+            header+=f"Host: {self.address}\r\n".encode('utf-8')
+            header += b"\r\n"
+            with open("index.html", "r") as file:
+                content=file.read()
+                header += f'{content}'.encode('utf-8')
+                fd.send(header)            
             
         else:
-            self.error404()
+            self.error404(fd)
+
             
 
     def post_request(self):
@@ -56,9 +76,20 @@ class Http_server():
         assert False, "NOT Implemented"
     def delete_request(self, id):
         assert False, "NOT Implemented"
-    def error404(self):
-        assert False, "not Implemented"
+    def error404(self, fd):
+        body= b"<h>PAGE NOT FOUND</h1>"
+        content_len=len(body)
+        header=b"HTTP/1.1 404 Not Found\r\n"
+        header+=f"Host: {self.address}\r\n".encode('utf-8')
+        header +=b'Content-type: text/html\r\n'
+        header += f"Content-Length: {content_len}\r\n".encode("utf-8")
+        header += b"\r\n"
+        header += body
+        fd.send(header)
+        print("sended", header)
+
     def unsupported_http_version(self):
+
         print("==========================================")
         print("Unsported HTTP version                  ")
         print("We only support HTTP version 1.0 and 1.1")
